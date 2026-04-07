be4fc2aec3aee902b4c7c15095b8d21330ebbb90
SyspreeDev <117625916+SyspreeDev@users.noreply.github.com>
2026-04-07 15:39:13 +0530

    Update email transport configuration for security

--- api/send.js
+++ api/send.js
@@ -6,8 +6,8 @@ export async function POST(req) {
 
   const transporter = nodemailer.createTransport({
     host: "mx.easymail.ca",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
