88c0202a7dc13be6bb885e211937081191a70322
SyspreeDev <117625916+SyspreeDev@users.noreply.github.com>
2026-04-07 15:46:20 +0530

    port updated

--- api/send.js
+++ api/send.js
@@ -5,9 +5,9 @@ export async function POST(req) {
   const { name, email, phone, company, message } = await req.json();
 
   const transporter = nodemailer.createTransport({
-    host: "mx.easymail.ca",
-    port: 465,
-    secure: true,
+    host: "mail.easymail.ca",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
