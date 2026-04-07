a0eeadfe622ac4aa2e21235674b8a6a0d79927b2
SyspreeDev <development@syspreedigital.com>
2026-04-07 16:02:21 +0530

    final cta

--- api/send.js
+++ api/send.js
@@ -8,9 +8,7 @@ export async function POST(req) {
     host: "mx.easymail.ca",
     port: 587,
     secure: false,
-    host: "mail.easymail.ca",
-    port: 587,
-    secure: false,
+  
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
